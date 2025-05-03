83f55939f8435a806a95974a66eb3cd601f31765
Nathan Carlini <nathan1.carlini@gmail.com>
2025-05-03 14:06:00 +0200

    avant ajout heatmap

--- app/api/getfestivalsmap/route.ts
+++ app/api/getfestivalsmap/route.ts
@@ -3,7 +3,7 @@ import connection from "@/db/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const query = "SELECT * FROM festivalgeo LIMIT 20";
+    const query = "SELECT * FROM festivalgeo LIMIT 100";
   
     const festivalBulkList = await connection.query(query);
     //  console.log(festivalBulkList.rows)
