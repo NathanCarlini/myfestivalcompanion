a9edc30408a9d945236a5b056aa23efefc4fe848
Nathan Carlini <nathan1.carlini@gmail.com>
2025-06-01 10:22:26 +0200

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth, { AuthOptions, User } from "next-auth";
-import { PostgresAdapter } from "@auth/postgres-adapter";
+import PostgresAdapter from "../../ProcessFcts/pgAdapter"; // Import relatif
 
 declare module "next-auth" {
   interface Session {
