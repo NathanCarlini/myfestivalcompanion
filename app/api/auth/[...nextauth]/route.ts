1cca40e44fdef64da483e7c3960c9df9babc6505
Nathan Carlini <nathan1.carlini@gmail.com>
2025-06-01 11:15:04 +0200

    location change

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth, { AuthOptions, User } from "next-auth";
-import PostgresAdapter from "../../ProcessFcts/pgAdapter";
+import PostgresAdapter from "./pgAdapter";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
