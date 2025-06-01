7835583d2d520de2aebe95cd7c2db5b60f57f3a0
Nathan Carlini <nathan1.carlini@gmail.com>
2025-06-01 13:16:09 +0200

    supp old rte

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,18 +2,6 @@ import connection from "@/db/db";
 import NowDate from "@/app/Objects/Date";
 import { NextResponse } from "next/server";
 
-export async function createUser(name: string, email: string, password: string, imageprofil: string) {
-  const verif = `select * from account where email = '${email}'`
-  const verifEmail = await connection.query(verif);
-  if (verifEmail.rows.length > 0) {
-    return "Email already used";
-  } else {
-    const query = `INSERT INTO account (name, email, password, imageprofil, timestamp, creationDate) VALUES ('${name}', '${email}', '${password}', '${imageprofil}', ${Date.now()}, ${new NowDate().fulldate});`;
-    await connection.query(query);
-    return true;
-  }
-}
-
 // Handler pour GET ou POST selon besoin
 export async function POST(request: Request) {
   try {
