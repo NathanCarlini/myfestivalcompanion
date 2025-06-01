eb98935ad4aaa5473236eaadadcdd7c8683e773b
Nathan Carlini <nathan1.carlini@gmail.com>
2025-06-01 12:26:10 +0200

    fix

--- app/api/getfestivals/route.ts
+++ app/api/getfestivals/route.ts
@@ -10,17 +10,21 @@ export async function GET() {
   return NextResponse.json(festivalBulkList.rows);
 }
 
-export async function POST(argandvalues: Array<Object>){
-  console.log(argandvalues)
-  let query = `SELECT * FROM festival WHERE `;
-  argandvalues != null ? argandvalues.forEach((element)  => {
-      var cond = `${Object.keys(element)[0] = Object.values(element)[0]}`
-      if (argandvalues.length > 1 && argandvalues.indexOf(element) < argandvalues.length - 1) {
-        cond += ` AND `
-    }
-    query += cond + ` LIMIT 20 ;`
-  }) : query = `SELECT * FROM festival ;`;  
-  const festivalBulkMapInfos = await connection.query(query);
+export async function POST(
+  // argandvalues: Array<Object>
+){
+  // console.log(argandvalues)
+  // let query = `SELECT * FROM festival WHERE `;
+  // argandvalues != null ? argandvalues.forEach((element)  => {
+  //     var cond = `${Object.keys(element)[0] = Object.values(element)[0]}`
+  //     if (argandvalues.length > 1 && argandvalues.indexOf(element) < argandvalues.length - 1) {
+  //       cond += ` AND `
+  //   }
+  //   query += cond + ` LIMIT 20 ;`
+  // }) : query = `SELECT * FROM festival ;`;  
+  // const festivalBulkMapInfos = await connection.query(query);
    
-  return  NextResponse.json(festivalBulkMapInfos.rows);
+  // return  NextResponse.json(festivalBulkMapInfos.rows);
+  return  NextResponse.json(success : true);
+
 }
